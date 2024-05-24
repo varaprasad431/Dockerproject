9749188785f63ef73e4ce22b8f5c057f4118327e
varaprasad431 <147475432+varaprasad431@users.noreply.github.com>
2024-05-24 11:59:10 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,8 @@
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo # Corrected the flag to specify output file with -O
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key # Corrected the flag to import the GPG key
-sudo amazon-linux-extras install java-openjdk11 -y # Removed 'yum install' which is redundant and should be a separate command
-sudo yum install git maven jenkins -y # Combined 'git', 'maven', and 'jenkins' into one 'yum install' command
-sudo systemctl start jenkins.service # Started Jenkins service
-sudo systemctl status jenkins.service # Checked status of Jenkins service
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo 
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+sudo amazon-linux-extras install java-openjdk11 -y 
+sudo yum install git maven jenkins -y
+
+sudo systemctl start jenkins.service 
+sudo systemctl status jenkins.service
